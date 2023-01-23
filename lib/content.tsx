6f8dfed1d748b4ea7a338fe61cc74ef92c25e553
dstokesy <danielstokes93@gmail.com>
2023-01-23 17:40:16 +0000

    adds amends

---
+++ lib/content.tsx
@@ -0,0 +1,4 @@
+// import { content as homeContent } from './../content/home';
+// import { content as frontendContent } from './../content/frontend';
+
+export async function getContentData(id: string) {}
